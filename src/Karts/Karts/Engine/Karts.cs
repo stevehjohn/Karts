3e860dea166e899efa9b070d24e85c7df7c74069
Stevo <stevehuwjohn@gmail.com>
2024-01-20 12:35:13 +0000

    Wip

--- src/Karts/Karts/Engine/Karts.cs
+++ src/Karts/Karts/Engine/Karts.cs
@@ -1,7 +1,34 @@
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Karts.Engine;
 
 public class Karts : Game
 {
+    private GraphicsDeviceManager _graphicsDeviceManager;
+    
+    private SpriteBatch _spriteBatch;
+    
+    public Karts()
+    {
+        _graphicsDeviceManager = new GraphicsDeviceManager(this)
+        {
+            PreferredBackBufferWidth = 1150,
+            PreferredBackBufferHeight = 1150
+        };
+
+        Content.RootDirectory = "./_Content";
+    }
+    
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+        
+        base.LoadContent();
+    }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
 }
