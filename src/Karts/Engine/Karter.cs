1d4b0fb9b66c4be2d6cdbc13c090df47a2b05012
Stevo John <stevehuwjohn@gmail.com>
2024-01-23 11:49:07 +0000

    Wip

--- src/Karts/Engine/Karter.cs
+++ src/Karts/Engine/Karter.cs
@@ -1,3 +1,4 @@
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,6 +9,12 @@ public class Karter : IActor
 {
     private Texture2D _texture;
 
+    private int _offset;
+
+    private readonly Random _rng = new();
+
+    private int _frame;
+    
     public void LoadContent(ContentManager contentManager)
     {
         _texture = contentManager.Load<Texture2D>("mario");
@@ -15,6 +22,12 @@ public class Karter : IActor
 
     public void Update()
     {
+        if (_frame % 3 == 0)
+        {
+            _offset = _rng.Next(2) * 2;
+        }
+
+        _frame++;
     }
 
     public void Draw(SpriteBatch spriteBatch)
@@ -22,7 +35,7 @@ public class Karter : IActor
         // ReSharper disable once PossibleLossOfFraction
         spriteBatch.Draw(
             _texture,
-            new Vector2(Constants.BufferWidth / 2 - 64, (int) (Constants.BufferHeight * .65)),
+            new Vector2(Constants.BufferWidth / 2 - 64, (int) (Constants.BufferHeight * .65) + _offset),
             new Rectangle(0, 0, 31, 31), Color.White,
             0f,
             Vector2.Zero,
